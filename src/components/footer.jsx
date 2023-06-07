724159a8322d2f4baf254e4cb73ddb7c883fe1ac
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-06-07 15:13:24 +0530

    Add footer section.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer section</div>;
+  return (
+    <div className="bg-gray-100 h-auto w-full py-5 flex items-center justify-center text-blue-950 font-semibold text-center px-8">
+      © 2023 Chamod Dananjaya | Full Stack Web Developer | Sri Lanka
+    </div>
+  );
 };
 
 export default Footer;
